8f0e90eea8adbd45b207368286a725f6b82a2f55
Hardik Bhammar <94770400+Hardik8491@users.noreply.github.com>
2023-08-30 10:05:08 +0530

    second commint

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
 
-import { store } from "./store/store";
+import { store } from './Store/Store';
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
